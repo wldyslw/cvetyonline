9779e50a276a35662b81e7c0f34108380ed3b25d
Vladimir Sernatsky <byport1112@gmail.com>
2017-07-30 19:57:26 +0300

    dev/api: images
    but not fully completed

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ApplicationRecord
+  has_attached_file :image, styles: (lambda do |attachment|
+    Hash.new.tap do |h|
+      h[:high] = '1024x768>'
+      h[:medium] = '256x192>'
+      h[:thumb] = '64x64>'
+    end
+  end)
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  def as_json(options)
+    Hash.new.tap do |h|
+      %i[id name description category price in_stock featured].each { |key| h[key] = send(key) }
+      h[:image_paths] = %i[high medium thumb].map { |style| [style, image.url(style)] }.to_h
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170728163326) do
+ActiveRecord::Schema.define(version: 20170729141724) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -47,12 +47,15 @@ ActiveRecord::Schema.define(version: 20170728163326) do
     t.string "name"
     t.text "description"
     t.string "category"
-    t.string "image_path"
     t.decimal "price", precision: 10, scale: 2
     t.boolean "in_stock"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "featured", default: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
