7433dc159902b8417f6a5990b92660c14c5748bd
Vladimir Sernatsky <byport1112@gmail.com>
2017-08-04 20:22:05 +0300

    dev/api: image styles
    now depends on featured

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   has_attached_file :image, styles: (lambda do |attachment|
     Hash.new.tap do |h|
-      h[:high] = '1024x768>'
+      h[:high] = '1024x768>' if attachment.instance.featured
       h[:medium] = '256x192>'
       h[:thumb] = '64x64>'
     end
@@ -11,7 +11,7 @@ class Product < ApplicationRecord
   def as_json(options)
     Hash.new.tap do |h|
       %i[id name description category price in_stock featured].each { |key| h[key] = send(key) }
-      h[:image_paths] = %i[high medium thumb].map { |style| [style, image.url(style)] }.to_h
+      h[:image_paths] = image.styles.keys.map { |style| [style, image.url(style)] }.to_h
     end
   end
 end
