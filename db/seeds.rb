018da52b490a00f1c163ef0389d049acdacacc46
Vladimir Sernatsky <byport1112@gmail.com>
2017-08-18 18:38:06 +0300

    dev/admin: production support

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+AdminUser.create!(Rails.env.production? ? { email: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD'], password_confirmation: ENV['ADMIN_PASSWORD'] } : { email: 'admin@example.com', password: 'password', password_confirmation: 'password' })
