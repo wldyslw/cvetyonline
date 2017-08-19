5c808e2512038932cd350122de136749b0af2fa0
Vladimir Sernatsky <byport1112@gmail.com>
2017-08-19 14:37:44 +0300

    dev/api: client name for orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170817162146) do
+ActiveRecord::Schema.define(version: 20170819112227) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20170817162146) do
     t.text "comment", default: ""
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "client_name"
   end
 
   create_table "products", force: :cascade do |t|
