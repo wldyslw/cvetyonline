683bbc901852d46319aa5d00829efc79bc66b59a
wldyslw <wldyslw@outlook.com>
2017-08-26 18:51:21 +0300

    dev/client: redundant logging removed

--- client/actions.js
+++ client/actions.js
@@ -32,7 +32,7 @@ export const fetchFlowers = request => dispatch => {
     return fetch(fullRequest, {method: 'GET'})
     .then(data => data.json(), err => console.log(`An error occured while fetching of request ${fullRequest}: ${err}`))
     .then(json => {
-        console.log(json);
+        // console.log(json);
         return dispatch(recieveFlowers(json.length === undefined ? Array.of(json) : json))
     } )
 }
