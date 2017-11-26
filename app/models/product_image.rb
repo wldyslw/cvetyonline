2a425782ac78f15ba0241da6cd24cb7535f99f7c
wldyslw <wldyslw@outlook.com>
2017-11-26 18:24:49 +0300

    dev: image aspect ratio changed (sic!)

--- app/models/product_image.rb
+++ app/models/product_image.rb
@@ -1,6 +1,6 @@
 class ProductImage < ApplicationRecord
   belongs_to :product
 
-  has_attached_file :image, use_timestamp: false, styles: { high: '1200x900#', medium: '600x450#', thumb: '64x64#' }
+  has_attached_file :image, use_timestamp: false, styles: { high: '1200x900#', medium: '450x450#', thumb: '64x64#' }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
